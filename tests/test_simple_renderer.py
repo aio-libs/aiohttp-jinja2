a956f1a3d018c0f2e3ab00a279ef98e812eebd73
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-02-15 13:05:28 +0200

    Add test for render_template

--- tests/test_simple_renderer.py
+++ tests/test_simple_renderer.py
@@ -189,3 +189,36 @@ class TestSimple(unittest.TestCase):
             self.addCleanup(srv.close)
 
         self.loop.run_until_complete(go())
+
+    def test_render_template(self):
+
+        @asyncio.coroutine
+        def func(request):
+            return aiohttp_jinja2.render_template(
+                'tmpl.jinja2', request,
+                {'head': 'HEAD', 'text': 'text'})
+
+        @asyncio.coroutine
+        def go():
+            app = web.Application(loop=self.loop)
+            aiohttp_jinja2.setup(app, loader=jinja2.DictLoader(
+                {'tmpl.jinja2':
+                 "<html><body><h1>{{head}}</h1>{{text}}</body></html>"}))
+
+            app.router.add_route('GET', '/', func)
+
+            port = self.find_unused_port()
+            srv = yield from self.loop.create_server(
+                app.make_handler(), '127.0.0.1', port)
+            url = "http://127.0.0.1:{}/".format(port)
+
+            resp = yield from aiohttp.request('GET', url, loop=self.loop)
+            self.assertEqual(200, resp.status)
+            txt = yield from resp.text()
+            self.assertEqual('<html><body><h1>HEAD</h1>text</body></html>',
+                             txt)
+
+            srv.close()
+            self.addCleanup(srv.close)
+
+        self.loop.run_until_complete(go())
