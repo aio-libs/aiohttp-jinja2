acd9fdbed330cfc23699f935219e743e6649309c
Kamil Sokołowski <sokolowski.k@outlook.com>
2017-04-05 02:53:22 +0200

    Allow to pass custom status code in render_template function

--- tests/test_simple_renderer.py
+++ tests/test_simple_renderer.py
@@ -187,6 +187,33 @@ def test_render_template(loop, test_client):
 
 
 @asyncio.coroutine
+def test_render_template_custom_status(loop, test_client):
+
+    @asyncio.coroutine
+    def func(request):
+        return aiohttp_jinja2.render_template(
+            'tmpl.jinja2', request,
+            {'head': 'HEAD', 'text': 'text'}, status=404)
+
+    template = '<html><body><h1>{{head}}</h1>{{text}}</body></html>'
+
+    app = web.Application(loop=loop)
+    aiohttp_jinja2.setup(app, loader=jinja2.DictLoader({
+        'tmpl.jinja2': template
+    }))
+
+    app.router.add_route('*', '/', func)
+
+    client = yield from test_client(app)
+
+    resp = yield from client.get('/')
+
+    assert 404 == resp.status
+    txt = yield from resp.text()
+    assert '<html><body><h1>HEAD</h1>text</body></html>' == txt
+
+
+@asyncio.coroutine
 def test_template_not_found(loop):
 
     @asyncio.coroutine
