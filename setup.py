695ba2bc7e7342e9f53f282025fef0ee83cfd4f3
Misha Behersky <bmwant@gmail.com>
2017-06-27 20:42:35 +0300

    Change codestyle

--- setup.py
+++ setup.py
@@ -26,26 +26,26 @@ tests_require = install_requires + [
 
 setup(
     name='aiohttp-jinja2',
-      version=version,
-      description=("jinja2 template renderer for aiohttp.web "
-                   "(http server for asyncio)"),
-      long_description='\n\n'.join((read('README.rst'), read('CHANGES.txt'))),
-      classifiers=[
-          'License :: OSI Approved :: Apache Software License',
-          'Intended Audience :: Developers',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Topic :: Internet :: WWW/HTTP'],
-      author='Andrew Svetlov',
-      author_email='andrew.svetlov@gmail.com',
-      url='https://github.com/aio-libs/aiohttp_jinja2/',
-      license='Apache 2',
-      packages=['aiohttp_jinja2'],
-      install_requires=install_requires,
+    version=version,
+    description=("jinja2 template renderer for aiohttp.web "
+                 "(http server for asyncio)"),
+    long_description='\n\n'.join((read('README.rst'), read('CHANGES.txt'))),
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP'],
+    author='Andrew Svetlov',
+    author_email='andrew.svetlov@gmail.com',
+    url='https://github.com/aio-libs/aiohttp_jinja2/',
+    license='Apache 2',
+    packages=['aiohttp_jinja2'],
+    install_requires=install_requires,
     extras_require={
         'test': tests_require,
     },
-  include_package_data=True
+    include_package_data=True
 )
