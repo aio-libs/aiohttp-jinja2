54dfedc9dbc767cfa0326f955f670dd0624b429f
Andrew Svetlov <andrew.svetlov@gmail.com>
2018-04-11 10:46:57 +0300

    Bump to 1.0.0

--- aiohttp_jinja2/__init__.py
+++ aiohttp_jinja2/__init__.py
@@ -8,7 +8,7 @@ from aiohttp.abc import AbstractView
 from .helpers import GLOBAL_HELPERS
 
 
-__version__ = '0.17.0'
+__version__ = '1.0.0'
 
 __all__ = ('setup', 'get_env', 'render_template', 'render_string', 'template')
 
