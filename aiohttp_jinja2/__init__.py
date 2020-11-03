3b1178f1dc33335af66df450a34f6bd7ebe15b47
Sam Bull <git@sambull.org>
2020-11-03 22:43:33 +0000

    Fix fallback.

--- aiohttp_jinja2/__init__.py
+++ aiohttp_jinja2/__init__.py
@@ -61,7 +61,7 @@ if sys.version_info >= (3, 8):
 
 
 else:
-    _TemplateWrapped = Callable[[Any, ...], web.StreamResponse]
+    _TemplateWrapped = Callable[[...], web.StreamResponse]
 
 
 def setup(
